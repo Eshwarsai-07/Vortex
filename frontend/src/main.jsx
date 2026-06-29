a917acefc751afd8a7e5c1c21be61def1772275b
Eshwarsai-07 <eashwarsaiga@gmail.com>
2026-06-29 14:43:16 +0530

    fix: set explicit global axios baseURL to live AWS backend 52.45.15.38:5005

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,9 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './redux/store';
 import AutoLogout from './components/AutoLogout.jsx';
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://52.45.15.38:5005';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
